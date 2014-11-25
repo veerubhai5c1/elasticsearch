8a92d7a0b44715ea03ace4e888aa8a951003f514
Veeresh <v.veeresh@thinkcloud.in>
2014-11-25 10:09:28 +0530

    Changes for multi host cluster

--- esnode/startNode.sh
+++ esnode/startNode.sh
@@ -4,9 +4,18 @@ NODE_NAME=${NODE_NAME:="node"}
 CLUSTER_NAME=${CLUSTER_NAME:="elasticsearch"}
 IS_MASTER=${IS_MASTER:=false}
 IS_DATA=${IS_DATA:=true}
+DISCOVERY_STRING=${DISCOVERY_STRING}
+MINIMUM_MASTERS=${MINIMUM_MASTERS:=1}
+HOST_IP=${HOST_IP}
+TCP_PORT=${TCP_PORT:=9300}
+PUBLISH_IP=${PUBLISH_IP}
+HTTP_PORT=${HTTP_PORT:=9200}
 
 # Replace the names and config before starting elasticsearch
-sed -i -e "/cluster.name:*/c\cluster.name: ${CLUSTER_NAME}" -e "/node.name:*/c\node.name: ${NODE_NAME}" -e "/node.data:*/c\node.data: ${IS_DATA}" -e  "/node.master:*/c\node.master: ${IS_MASTER}"  /elasticsearch/config/elasticsearch.yml
+sed -i -e "/cluster.name:*/c\cluster.name: ${CLUSTER_NAME}" -e "/node.name:*/c\node.name: ${NODE_NAME}" -e "/node.data:*/c\node.data: ${IS_DATA}" -e  "/node.master:*/c\node.master: ${IS_MASTER}" -e "s/DISCOVERY_STRING/$DISCOVERY_STRING/g"   -e "s/MINIMUM_MASTERS/$MINIMUM_MASTERS/g" -e "s/HOST_IP/$HOST_IP/g" -e "s/TCP_PORT/$TCP_PORT/g" -e "s/PUBLISH_IP/$PUBLISH_IP/g" -e "s/HTTP_PORT/$HTTP_PORT/g" /elasticsearch/config/elasticsearch.yml
+
+cat /elasticsearch/config/elasticsearch.yml
+
 
 # Start the elasticsearch node
 /elasticsearch/bin/elasticsearch
--- start_cluster.sh
+++ start_cluster.sh
@@ -4,7 +4,11 @@ sudo docker stop es2
 # Remove es docker containers
 sudo docker rm es1
 sudo docker rm es2
+DISCOVERY_STRING="[\"192.168.123.166[9300-9301]\"]"
+MINIMUM_MASTERS=1
+HOST_IP=_non_loopback_:ipv4_
+PUBLISH_IP=192.168.123.165
 # Start Master node
-sudo docker run --name es1 -d -P -v /var/log/elasticsearch/es1/data:/data -v /var/log/elasticsearch/es1/supervisor:/var/log/supervisor -e NODE_NAME=node.master -e CLUSTER_NAME=mycluster1 -e IS_MASTER=true -e IS_DATA=false veeresh/elasticsearch
+sudo docker run --name es1 -d -p 9200:9200 -p 9300:9300 -v /var/log/elasticsearch/es1/data:/data -v /var/log/elasticsearch/es1/supervisor:/var/log/supervisor -e NODE_NAME=node.master.165 -e CLUSTER_NAME=mycluster1 -e IS_MASTER=true -e IS_DATA=false -e DISCOVERY_STRING=$DISCOVERY_STRING -e MINIMUM_MASTERS=$MINIMUM_MASTERS -e HOST_IP=$HOST_IP -e TCP_PORT=9300 -e PUBLISH_IP=$PUBLISH_IP -e HTTP_PORT=9200 registry.nirmata.com/nirmata/elasticsearch
 # Start data node
-sudo docker run --name es2 -d -P -v /var/log/elasticsearch/es2/data:/data -v /var/log/elasticsearch/es2/supervisor:/var/log/supervisor -e NODE_NAME=node.data -e CLUSTER_NAME=mycluster1 -e IS_MASTER=false -e IS_DATA=true veeresh/elasticsearch
+#sudo docker run --name es2 -d -p 9201:9200 -p 9301:9300 -v /var/log/elasticsearch/es2/data:/data -v /var/log/elasticsearch/es2/supervisor:/var/log/supervisor -e NODE_NAME=node.data.165 -e CLUSTER_NAME=mycluster1 -e IS_MASTER=false -e IS_DATA=true -e DISCOVERY_STRING=$DISCOVERY_STRING registry.nirmata.com/nirmata/elasticsearch
